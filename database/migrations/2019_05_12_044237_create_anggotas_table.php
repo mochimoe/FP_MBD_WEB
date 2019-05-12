f16e9f046307414f44b56d0bf04ea0374fede3aa
Aisyah Tria <aisyahnyawn@gmail.com>
2019-05-12 12:14:41 +0700

    nambahin model tp masih error

---
+++ database/migrations/2019_05_12_044237_create_anggotas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnggotasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anggotas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('id_anggota');
+            $table->string('nama_anggota');
+            $table->date('tgl_lahir');
+            $table->date('tgl_daftar');
+            $table->string('kelamin');
+            $table->string('alamat');
+            $table->string('no_hp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anggotas');
+    }
+}
