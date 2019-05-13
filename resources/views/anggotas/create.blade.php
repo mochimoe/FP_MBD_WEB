c6d48e6516e4b76ae508b50d83d698c4b8f2b9a6
Aisyah Tria <aisyahnyawn@gmail.com>
2019-05-13 07:36:24 +0700

    fix navbar

--- resources/views/anggotas/create.blade.php
+++ resources/views/anggotas/create.blade.php
@@ -1,5 +1,3 @@
-<!-- create.blade.php -->
-
 @extends('layouts.app')
 
 @section('title')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,6 @@
                             {{-- @if (Route::has('login')) --}}
                         <ul class="nav-menu">
                                 <li class=""><a href="/">Home</a></li>
-                                <li><a href="about.html">About</a></li>
                                 <li class="menu-has-children"><a href="">Input</a>
                                     <ul>
                                             <li><a href="/anggotas/create">Anggota</a></li>
@@ -104,26 +103,26 @@
         </section>
     </div>
 
-    <script src="js/vendor/jquery-2.2.4.min.js"></script>
+    <script src="{{asset('js/vendor/jquery-2.2.4.min.js')}}"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
 	 crossorigin="anonymous"></script>
-	<script src="js/vendor/bootstrap.min.js"></script>
+	<script src="{{asset('js/vendor/bootstrap.min.js')}}"></script>
 	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
-	<script src="js/easing.min.js"></script>
-	<script src="js/hoverIntent.js"></script>
-	<script src="js/superfish.min.js"></script>
-	<script src="js/jquery.ajaxchimp.min.js"></script>
-	<script src="js/jquery.magnific-popup.min.js"></script>
-	<script src="js/owl.carousel.min.js"></script>
-	<script src="js/owl-carousel-thumb.min.js"></script>
-	<script src="js/jquery.sticky.js"></script>
-	<script src="js/jquery.nice-select.min.js"></script>
-	<script src="js/parallax.min.js"></script>
-	<script src="js/waypoints.min.js"></script>
-	<script src="js/wow.min.js"></script>
-	<script src="js/jquery.counterup.min.js"></script>
-	<script src="js/mail-script.js"></script>
-	<script src="js/main.js"></script>
+	<script src="{{asset('js/easing.min.js')}}"></script>
+	<script src="{{asset('js/hoverIntent.js')}}"></script>
+	<script src="{{asset('js/superfish.min.js')}}"></script>
+	<script src="{{asset('js/jquery.ajaxchimp.min.js')}}"></script>
+	<script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+	<script src="{{asset('js/owl.carousel.min.js')}}"></script>
+	<script src="{{asset('js/owl-carousel-thumb.min.js')}}"></script>
+	<script src="{{asset('js/jquery.sticky.js')}}"></script>
+	<script src="{{asset('js/jquery.nice-select.min.js')}}"></script>
+	<script src="{{asset('js/parallax.min.js')}}"></script>
+	<script src="{{asset('js/waypoints.min.js')}}"></script>
+	<script src="{{asset('js/wow.min.js')}}"></script>
+	<script src="{{asset('js/jquery.counterup.min.js')}}"></script>
+	<script src="{{asset('js/mail-script.js')}}"></script>
+	<script src="{{asset('js/main.js')}}"></script>
     
 </body>
 </html>
